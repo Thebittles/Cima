0b26284af33780fee19b62f9296953fff829b370
Jesus Nunez Xoconoxtle <jesusnunezxoco@gmail.com>
2021-04-18 23:41:42 -0500

    minor errors

--- app/public/script.js
+++ app/public/script.js
@@ -6,11 +6,11 @@ const todoUrl = 'http://localhost:3000/dashboard'
 $('.symptom-list').on('click', 'span', function(event){
     event.stopPropagation();
     var self = this; 
-    console.log('I am self: ', self)
+    
     
     var thisId = $(this).parent().data('id');
-    console.log('I am the id ', thisId)
-    var url = `${todoUrl}/${thisId}`;
+    
+    var url = `${todoUrl}/symptom/${thisId}`
     $.ajax({
         url: url,
         method: 'DELETE',
